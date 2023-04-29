35fa6cda35e71713869e6c468658bb6dd039f1a4
guhmerces <guhmerces@hotmail.com>
2023-04-28 22:41:40 -0300

    Create CreateBookRequest file

---
+++ app/Http/Requests/CreateBookRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'isbn' => 'required|numeric',
+            'value' => 'required|regex:/^\d+(\.\d{1,2})?$/', // money - examples: 10, 10.5, 10.05
+        ];
+    }
+}
