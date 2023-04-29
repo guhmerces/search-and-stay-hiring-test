c498da9c5798ee3544f53620040fdd8b03e22977
guhmerces <guhmerces@hotmail.com>
2023-04-29 05:25:07 -0300

    Add helpers related to auth to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function get_auth_http_header(User $user): array
+    {
+        $token = $user->createToken('Test')->plainTextToken;
+
+        return [
+            'Authorization' => 'Bearer ' . $token,
+        ];
+    }
+
+    public function create_user(): User
+    {
+        User::factory()->make()->save();
+
+        return User::first();
+    }
 }
