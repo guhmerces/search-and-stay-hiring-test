de4674f55aacf978202313742c3d388c636c6c2f
guhmerces <guhmerces@hotmail.com>
2023-04-29 05:01:53 -0300

    Create RegisterUserRequest

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|max:256',
+            'password' => 'required|string|max:256|min:8',
+            'password_confirmation' => 'required_with:password|same:password'
+        ];
+    }
+}
